575ac9be6539fe6ada56a73db0e6460b6fd48129
alexzhangryan <alexzhangryan@gmail.com>
2025-09-19 21:48:18 -0500

    implemented model

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 # %%
-from re import L
+from matplotlib import pyplot as plt
 import pandas as pd
 import textwrap
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.preprocessing import OneHotEncoder
+import numpy as np
+from sklearn.metrics import classification_report
 
 
 df = pd.read_csv("match_data.csv")
@@ -21,8 +26,13 @@ red = red.add_prefix("red_")
 blue = blue.rename(columns={"blue_gameid": "gameid"})
 red = red.rename(columns={"red_gameid": "gameid"})
 
+
+blue_ids = set(blue["gameid"])
+red_ids = set(red["gameid"])
+
 matches = pd.merge(blue, red, on="gameid", suffixes=("",""))
-print(teams2025.head(10).to_string())
+matches["blue_elo"] = 1500
+matches["red_elo"] = 1500
 region_strength2025 = {
     "LCK": 1755,
     "LPL": 1604,
@@ -31,6 +41,9 @@ region_strength2025 = {
     "LCP": 1294,
     "LTA S": 824
 }
+training_data = matches.drop(columns=["gameid", "red_result"])
+result_column = training_data.pop("blue_result")
+training_data.insert(0, "blue_result", result_column)
 elo = {}
 region = {}
 winrate = {}
@@ -66,26 +79,64 @@ def initializeElo(matches):
             pass
         elo[blue_team] = elos[0]
         elo[red_team] = elos[1]
+    for gameid, match in matches.groupby("gameid"):
+        blue_team = match["blue_teamname"].values[0]
+        red_team = match["red_teamname"].values[0]
+        matches.loc[match.index, "blue_elo"] = elo.get(blue_team)
+        matches.loc[match.index, "red_elo"] = elo.get(red_team)
+        
+        
 
 #%%
 def initializeTeams(teams):
     for team in teams.itertuples():
-        elo[team[12]] = 1500
+        elo[team[8]] = 1500
         if team[2] != "FST" or team[2] != "EWC" or team[2] != "MSI" or team[2] != "WLDs":
             region[team[12]] = team[2]
             
 
 def initializeWinRates(teams):
     for team in teams.itertuples():
-        winrate[team[12]] = 0
+        winrate[team[8]] = 0
     for gameid, match in teams.groupby("gameid"):
-        print(match)
+        pass
 
 initializeTeams(teams2025)
+
+
 initializeElo(matches)
 sorted_elo = sorted(elo.items())
 elo_string = str(sorted_elo)
-print(textwrap.fill(elo_string, width=200))
+#print(textwrap.fill(elo_string, width=200))
+#elo_string = str(elo)
+#print(textwrap.fill(elo_string, width=200))
+#print(matches.head(10).to_string)
 
 
-# %%
+#print(training_data.columns.tolist())
+encoded_data = pd.get_dummies(training_data, dtype=int)
+#print(encoded_data.to_string())
+np.set_printoptions(threshold=np.inf, linewidth=np.inf)
+
+X = encoded_data.iloc[:, 1:]
+y = encoded_data.iloc[:, 0]
+
+#print(training_data.to_string())
+#%%
+X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=17, test_size=0.2)
+rf = RandomForestClassifier()
+rf.fit(X_train, y_train)
+
+y_pred = rf.predict(X_test)
+print(len(y))
+print(len(y_pred))
+rf.score(X_test, y_test)
+print(classification_report(y_test, y_pred))
+features = pd.DataFrame({
+    "importance": rf.feature_importances_, 
+    "feature": X.columns
+})
+sorted_features = features.sort_values(by="importance", ascending=False)
+print(sorted_features.to_string())
+
+#%%
